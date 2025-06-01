48874b307b94a49a241c518c1ee57a6f0ca0ce07
kalesha58 <bablu05786bablu@gmail.com>
2025-06-01 13:01:55 +0530

    fix the cors issue

--- api/index.js
+++ api/index.js
@@ -3,8 +3,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import dummyPermits from "../dummy.js";
 import chatRouter from "../routes/chat.js";
+import cors from "cors"; 
+
 const app = express();
 app.use(bodyParser.json());
+
+const allowedOrigins = ['https://ptw-agent-i5nb.vercel.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
+app.options('*', cors());
 
 app.use('/api/chat', chatRouter);
 
